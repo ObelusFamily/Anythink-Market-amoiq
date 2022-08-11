a4b5aab39989d9bde6fa6cf7be8b180a71b741bd
Rohit Yandigeri <rohityandigeri20042gmail.com>
2022-08-11 19:09:07 +0530

    added a new feature

--- frontend/src/components/Home/Banner.js
+++ frontend/src/components/Home/Banner.js
@@ -1,15 +1,36 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import logo from "../../imgs/logo.png";
 import { AiOutlineSearch } from "react-icons/ai";
 
 const Banner = (props) => {
+  const searchRef = useRef();
+  const searchBoxRef = useRef();
+
+  function toggleSearchBox() {
+    searchBoxRef.current.classList.toggle("hide");
+  }
+
+  useEffect(() => {
+    searchRef.current.addEventListener("click", toggleSearchBox);
+
+    return () => {
+      // eslint-disable-next-line react-hooks/exhaustive-deps
+      searchRef.current.removeEventListener("click", toggleSearchBox);
+    };
+  }, []);
   return (
     <div className="banner text-white">
       <div className="container p-4 text-center">
         <img src={logo} alt="banner" />
         <div>
-          <span id="get-part">A place to get</span>
-          <span className="mx-2 mw-25 position-relative">
+          <span ref={searchRef} id="get-part">
+            A place to get
+          </span>
+          <span
+            id="search-box-wrapper"
+            className="mx-2 mw-25 position-relative hide"
+            ref={searchBoxRef}
+          >
             <input
               id="search-box"
               style={{ minWidth: "400px" }}
